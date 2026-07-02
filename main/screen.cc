d2fd4c23b85f94879f3e296528ea17488286ef82
muz <171587778+MuzammilAijaz@users.noreply.github.com>
2026-07-03 02:02:03 +0500

    feat(UI): change startup screen to an animation that plays for 5s

--- main/screen.cc
+++ main/screen.cc
@@ -21,50 +21,37 @@ extern "C" {
 u8g2_t u8g2; // Global instance for Lopaka graphics code
 
 // [BEGIN lopaka generated]
-static const uint8_t image_BAT_percent_bits[] = {0x0c,0x03,0x1e,0x03,0x33,0x03,0xb3,0x03,0xde,0x01,0xec,0x00,0x70,0x00,0x38,0x00,0xdc,0x00,0xee,0x01,0x37,0x03,0x33,0x03,0xe3,0x01,0xc3,0x00};
-static const uint8_t image_battery_charging_bits[] = {0x00,0x40,0x00,0xf0,0x27,0x7f,0x08,0x30,0x80,0x08,0x10,0x80,0x0e,0x18,0x80,0x01,0x0c,0x80,0x01,0xfc,0x81,0x01,0xfe,0x80,0x01,0xc0,0x80,0x01,0x60,0x80,0x0e,0x20,0x80,0x08,0x30,0x80,0x08,0x10,0x80,0xf0,0xcb,0x7f,0x00,0x08,0x00,0x00,0x00,0x00};
-static const uint8_t image_CPU_bar_bits[] = {0xfc,0xff,0xff,0xff,0x01,0x02,0x00,0x00,0x00,0x02,0x01,0x00,0x00,0x00,0x04,0x01,0x00,0x00,0x00,0x04,0x01,0x00,0x00,0x00,0x04,0x01,0x00,0x00,0x00,0x04,0x01,0x00,0x00,0x00,0x04,0x02,0x00,0x00,0x00,0x02,0xfc,0xff,0xff,0xff,0x01};
+#include <stdint.h>
+#include "esp_timer.h"
+#include "lopaka_64_64_28f_copy_1.h"
+#include "lopaka_64_64_28f_copy_3.h"
+
+void updateAnimations(void) {
+    uint32_t now = (uint32_t)(esp_timer_get_time() / 1000);
+    lopaka_64_64_28f_copy_1_frame = now / 83 % 24;
+    lopaka_64_64_28f_copy_3_frame = (now / 83 + 7) % 24; // to avoid sync
+}
+
+void drawAnimation_lopaka_64_64_28f_copy_1(u8g2_t *u8g2) {
+    u8g2_SetDrawColor(u8g2, 1);
+    u8g2_DrawXBMP(u8g2, -1, 0, 64, 64, lopaka_64_64_28f_copy_1_frames[lopaka_64_64_28f_copy_1_frame]);
+}
+void drawAnimation_lopaka_64_64_28f_copy_3(u8g2_t *u8g2) {
+    u8g2_SetDrawColor(u8g2, 1);
+    u8g2_DrawXBMP(u8g2, 64, -1, 64, 64, lopaka_64_64_28f_copy_3_frames[lopaka_64_64_28f_copy_3_frame]);
+}
 
 void drawScreen_1(void) {
-  u8g2_ClearBuffer(&u8g2);
-  u8g2_SetBitmapMode(&u8g2, 1);
-  u8g2_SetFontMode(&u8g2, 1);
+    u8g2_ClearBuffer(&u8g2);
+    u8g2_SetBitmapMode(&u8g2, 1);
+    u8g2_SetFontMode(&u8g2, 1);
 
-  // RAM title
-  u8g2_SetFont(&u8g2, u8g2_font_t0_17b_tr);
-  u8g2_DrawStr(&u8g2, 51, 16, "RAM");
-  // RAM used
-  u8g2_DrawStr(&u8g2, 46, 37, "16");
-  // RAM seperator
-  u8g2_DrawStr(&u8g2, 75, 37, "/");
-  // RAM total
-  u8g2_DrawStr(&u8g2, 46, 55, "16");
-  // RAM total gb
-  u8g2_DrawStr(&u8g2, 65, 55, "GB");
-  // BAT title
-  u8g2_DrawStr(&u8g2, 94, 16, "BAT");
-  // BAT charge
-  u8g2_DrawStr(&u8g2, 103, 38, "100");
-  // BAT percent
-  u8g2_DrawXBM(&u8g2, 91, 25, 10, 14, image_BAT_percent_bits);
-  // battery_charging
-  u8g2_DrawXBM(&u8g2, 96, 45, 24, 16, image_battery_charging_bits);
-  // CPU title
-  u8g2_DrawStr(&u8g2, 7, 16, "CPU");
-  // CPU load
-  u8g2_DrawStr(&u8g2, 15, 38, "100");
-  // CPU percent
-  u8g2_DrawXBM(&u8g2, 3, 25, 10, 14, image_BAT_percent_bits);
-  // CPU bar
-  u8g2_DrawXBM(&u8g2, 4, 47, 35, 9, image_CPU_bar_bits);
-  // CPU bar filling
-  u8g2_DrawBox(&u8g2, 6, 49, 28, 5);
-  // barrier left
-  u8g2_DrawLine(&u8g2, 42, 0, 42, 64);
-  // barrier right
-  u8g2_DrawLine(&u8g2, 86, 0, 86, 64);
-  u8g2_SendBuffer(&u8g2);
+    drawAnimation_lopaka_64_64_28f_copy_1(&u8g2);
+    drawAnimation_lopaka_64_64_28f_copy_3(&u8g2);
+    u8g2_SendBuffer(&u8g2);
 }
+
+// Call updateAnimations() before drawScreen_1() in your ESP-IDF task loop.
 // [END lopaka generated]
 
 void init_display_u8g2(void *pvParameters) {
@@ -103,8 +90,18 @@ void init_display_u8g2(void *pvParameters) {
   u8g2_SetPowerSave(&u8g2, 0); // Wake up display panel RAM mapping
   u8g2_ClearBuffer(&u8g2);
 
-  // Execute your custom Lopaka screen UI layout
-  drawScreen_1();
+  // Execute screen UI layout
+  int64_t start_time = esp_timer_get_time(); // microseconds
+
+  while ((esp_timer_get_time() - start_time) < 5000000) { // 3s
+    updateAnimations();
+    drawScreen_1();
+    vTaskDelay(pdMS_TO_TICKS(30)); // ~33 FPS
+  }
+
+  // final clear frame
+  u8g2_ClearBuffer(&u8g2);
+  u8g2_SendBuffer(&u8g2);
 
   vTaskDelete(NULL);
 }
